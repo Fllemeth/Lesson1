0bee2b72272f8e39cad3a2e8e3d97836b27a4391
Fllemeth <tacshinbg@gmail.com>
2022-12-07 21:18:46 +0200

    1

--- src/practice3/Interval.java
+++ src/practice3/Interval.java
@@ -1,5 +1,24 @@
 package practice3;
 
+import java.util.Scanner;
+
 public class Interval {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if (num >= 0 && num <= 14) {
+            System.out.println("number betwen [0-14]");
+        }
+        else if (num >= 15 && num <= 35) {
+            System.out.println("number betwen [15-35]");
+        }
+        else if (num >= 36 && num <= 50) {
+            System.out.println("number betwen [36-50]");
+        }
+        else if (num >= 50 && num <= 100) {
+            System.out.println("number betwen [50-100]");
+        } else System.out.println("number is out of bounds");
+    }
+
 }
+
